05b7f3f6420c6625f5a42e62f0e6151aadb5061e
TouMing <lxlmeng@qq.com>
2025-01-11 02:29:11 +0800

    修复竖屏BUG

--- Assets/Script/MouseToWorldControl.cs
+++ Assets/Script/MouseToWorldControl.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using VInspector;
 
 public class MouseToWorldControl : MonoBehaviour
 {
@@ -9,6 +10,9 @@ public class MouseToWorldControl : MonoBehaviour
     public Camera camera;
     public float smoothTime = 0.3f;
     public float maxSpeed = 10;
+    [InspectorName("Y轴跟随限制")]
+    public Vector2 limitY = new Vector2(0.8f,0.6f);
+    [ReadOnly]
     public Vector3 velocity;
     public Vector4 offset = Vector4.one;
     private Vector3 targetPositionCache;
@@ -21,7 +25,6 @@ public class MouseToWorldControl : MonoBehaviour
     {
         control.position = targetPositionCache;
     }
-
     private void Update()
     {
         if(control == null || !MiSideStart.config.LookAtMouse)
@@ -29,8 +32,19 @@ public class MouseToWorldControl : MonoBehaviour
         var center = Screen.safeArea.center;
         var mousePos = (Vector2)Input.mousePosition;
         var distance = mousePos - center;
+      
         var mulX = distance.x < 0 ? offset.x : offset.z;
         var mulY = distance.y < 0 ? offset.y : offset.w;
+        var screenHeight = Screen.safeArea.height / 2;
+
+        if(distance.y > screenHeight * limitY.x)
+        {
+            distance.y = screenHeight * limitY.x;
+        }else if(distance.y < -screenHeight * limitY.y)
+        {
+            distance.y = -screenHeight * limitY.y;
+        }
+        Debug.Log(distance);
         mousePos = new Vector2(mulX, mulY) * distance;
         var position = camera.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, depth));
         control.position = Vector3.SmoothDamp(control.position, position, ref velocity, smoothTime, maxSpeed);
