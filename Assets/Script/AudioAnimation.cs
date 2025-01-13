fabda20e3932b1be618f4c7bf788d9514bb36e27
TouMing <lxlmeng@qq.com>
2025-01-13 18:22:38 +0800

    删除错误的PR

--- Assets/Script/AudioAnimation.cs
+++ Assets/Script/AudioAnimation.cs
@@ -1,30 +1,80 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using CSCore.SoundIn;
+using CSCore.Streams;
 using UnityEngine;
 using VInspector;
-using MitaMisiteAction.Nod;
 
 public class AudioAnimation : MonoBehaviour
 {
-    [SerializeField,ReadOnly]
-    private MitaNodController nodController;
-    [SerializeField,ReadOnly]
+    private WasapiLoopbackCapture capture;
+    private SoundInSource soundInSource;
+    private byte[] buffer;
+    [SerializeField]
+    private float[] audioSamples;
+    private int offset;
+    private int byteCount;
+   
     public MiSideStart miside;
-
+    public float nodEnergy;
+    [SerializeField,ReadOnly]
+    private float currentEnergy;
+    [SerializeField,ReadOnly]
+    private float disEnergy;
+    [SerializeField,ReadOnly]
+    private bool nod = false;
     private void Awake()
     {
-        nodController = new MitaNodController(startListen: true)
+        capture = new WasapiLoopbackCapture();
+        capture.Initialize();
+        capture.Start();
+        soundInSource = new SoundInSource(capture);
+        soundInSource.DataAvailable += OnDataAvailable;
     }
 
     private void Update()
     {
         if(!MiSideStart.config.MusicHead)
             return;
-        if (nodController.nod)
+        if (nod)
         {
             miside.NodOnShot();
+            nod = false;
         }
     }
+
+    private void OnDataAvailable(object sender, DataAvailableEventArgs e)
+    {
+        buffer = e.Data;
+        offset = e.Offset;
+        byteCount = e.ByteCount;
+        ParseBuffer();
+    }
+
+    private void ParseBuffer()
+    {
+        if (buffer == null || buffer.Length == 0)
+            return;
+
+        // 将 buffer 转换为浮点数据
+        int sampleCount = byteCount / sizeof(float);
+        int startCount = offset / sizeof(float);
+        int count = sampleCount-startCount;
+        audioSamples = new float[count];
+        float energy = 0;
+        for (int i = startCount; i < sampleCount; i++)
+        {
+            var sample = BitConverter.ToSingle(buffer, i * sizeof(float));
+            audioSamples[i] = sample;
+            energy += sample * sample; // 能量为振幅的平方和
+        }
+        energy /= count; // 平均能量
+        disEnergy = currentEnergy - energy;
+        currentEnergy = energy;
+        if (currentEnergy > nodEnergy && disEnergy > 0)
+            nod = true;
+    }
+  
 
 }
