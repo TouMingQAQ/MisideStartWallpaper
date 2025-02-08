2cb6dcb306c346799266374f69479831ed22abef
TouMing <lxlmeng@qq.com>
2025-02-08 18:43:53 +0800

    眨眼(未完成)

--- Assets/Script/MitaControl.cs
+++ Assets/Script/MitaControl.cs
@@ -1,4 +1,5 @@
 using System;
+using DG.Tweening;
 using RootMotion.FinalIK;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -7,6 +8,9 @@ using Random = UnityEngine.Random;
 public class MitaControl : MonoBehaviour,IPointerClickHandler
 {
     private static readonly int OnClick = Animator.StringToHash("OnClick");
+    private static readonly int Blink = Animator.StringToHash("Blink");
+    private static readonly int BlinkState = Animator.StringToHash("BlinkState");
+
     public MiSideStart start;
     public Animator animator;
     public LookAtIK lookAtIk;
@@ -15,7 +19,9 @@ public class MitaControl : MonoBehaviour,IPointerClickHandler
     public AudioSource audioSource;
     public AudioClip winkClip;
     public AudioClip onClickClip;
+    public SkinnedMeshRenderer headRender;
 
+    private bool _startAnimationEnd = false;
     private void Awake()
     {
         winkParticles = Instantiate(winkParticles,winkRoot);
@@ -24,6 +30,35 @@ public class MitaControl : MonoBehaviour,IPointerClickHandler
     {
         audioSource.PlayOneShot(winkClip);
         winkParticles.Play();
+    }
+
+    private void Update()
+    {
+        BlinkTimer();
+    }
+
+    private float blinkTimer = 0;
+    private int blinkShapeIndex = 0;
+    private int blinkWeight = 0;
+    void BlinkTimer()
+    {
+        if(!_startAnimationEnd)
+            return;
+        if (blinkTimer <= 0)
+        {
+            DoBlink();
+            blinkTimer = Random.Range(4f, 10f);
+        }
+        blinkTimer -= Time.deltaTime;
+    }
+    
+    public void DoBlink()
+    {
+        if(!_startAnimationEnd)
+            return;
+        var state = Random.Range(0, 2);
+        animator.SetTrigger(Blink);
+        animator.SetFloat(BlinkState,state);
     }
     public void OnPointerClick(PointerEventData eventData)
     {
@@ -55,5 +90,6 @@ public class MitaControl : MonoBehaviour,IPointerClickHandler
     public void OnStartAnimationEnd()
     {
         start.EnableControl();
+        _startAnimationEnd = true;
     }
 }
